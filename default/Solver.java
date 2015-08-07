77a815f04f5c71d79874cd7f5b7dd961f628d656
BrukArgaw <bkargaw@berkeley.edu>
2015-08-07 01:37:46 -0700

    only isvalidinput method is left

--- default/Solver.java
+++ default/Solver.java
@@ -1,3 +1,4 @@
+import java.awt.Point;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -18,7 +19,7 @@ public class Solver {
 		HashSet<String> visited;
 		Tray initTray; // update in isValidInput
  		
-		public Solver( ){
+		public Solver(){
 			//
 			lineInit = new ArrayList<String>();
 			lineGoal = new ArrayList<String>();
@@ -34,7 +35,7 @@ public class Solver {
 			 while (!fringe.isEmpty()){
 				 Tray X= fringe.pop();
 				 if (isGoal(X)){
-				    moves();
+				    moves(X);
 				 }else{
 					 for(Tray XChild: X.children){
 						 if (!fringe.contains(XChild) && !visited.contains(XChild.toString())){
@@ -57,17 +58,61 @@ public class Solver {
 		 */
 		
 		public boolean isGoal(Tray currTray){
-			
+			// lineGoal
+			int count=0;
+			int neededToBeGoal= lineGoal.size();
+			// for the Goal... 
+			int xtop;
+			int ytop;
+			int xbottom;
+			int ybottom;
+			for (String line : lineGoal) {
+				xtop = Integer.parseInt(line.split("\\s+")[1]);
+				ytop = Integer.parseInt(line.split("\\s+")[0]);
+				Point P= new Point(xtop,ytop);
+				// if we have the head in the current Tray
+				if (currTray.heads.contains(P)){
+					xbottom = Integer.parseInt(line.split("\\s+")[3]);
+					ybottom = Integer.parseInt(line.split("\\s+")[2]);
+					Point P1= new Point(xbottom,ybottom);
+					
+					int x= xtop+currTray.board[ytop][xtop].size.x-1;
+					int y= ytop+currTray.board[ytop][xtop].size.y-1;
+					Point PB= new Point(x,y);
+					// if the Bottom of the Goal matches the Bottom of the Given Block
+					if (P1.equals(PB)){
+						count++;
+					}
+				}
+			}
+			if (count== neededToBeGoal){
+				return true;
+			}
 			return false;
 		}
 		
 		/**
-		 * prints out all the moves from the start to Goal
+		 * prints out all the moves from the start to Goal (X)
+		 * @param X
+		 *          the Goal Tray
 		 */
-		public void moves(){
-			
-			
+		public void moves(Tray X){
+			Stack<String> S= new Stack<String>();
+			// avoid visiting the first tray since it has no moves
+			while(X.parentTray!=null){ 
+				S.push(X.moveFromParent);
+				X=X.parentTray;
+			}
+			while (!S.isEmpty()){
+				System.out.println(S.pop());
+			}
 		}
+		
+		/**
+		 * 
+		 * @param args
+		 * @return
+		 */
 		public boolean isValidInput(String[] args){
 			// check validity of args
 			
